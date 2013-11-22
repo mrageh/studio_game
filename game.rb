29cdc6981d094e0888cccfe541c8513448e60d59
Adam89 <Adamm1@aston.ac.uk>
2013-11-21 21:15:41 -0700

    basic command line game

---
+++ game.rb
@@ -0,0 +1,9 @@
+health = 60
+player_name1 = "larry"
+player_name2 = "curly"
+player_name3 = "moe"
+
+puts "#{player_name1}'s health is #{health / 9.0}"
+
+
+puts "Players: \n\t#{player_name1}\n\t#{player_name2}\n\t#{player_name3}"
