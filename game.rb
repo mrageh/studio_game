d5df3b4478122b8c6138b6cb2071cabf91a1dc76
Adam89 <Adamm1@aston.ac.uk>
2013-11-23 20:48:50 -0700

    finished basic method

--- game.rb
+++ game.rb
@@ -1,19 +1,12 @@
-time = Time.new
-current_time = time.strftime( "The game started on %A %d/%m/%Y %R%p")
-puts current_time
-health1 = 60
-health2 = health1
-health3 = 100
-name1 = "larry".capitalize
-name2 = "curly".upcase
-name3 = "moe".capitalize
-title3 = "#{name3} has a health of #{health3}.".center(50,"*")
-health1 = 30
-health4 = 90
-name4 = "shemp".capitalize
-title4 = "#{name4.ljust(30,".")} #{health4} health"
-puts "#{name1} has a health of #{health1}"
-puts "#{name2} has a health of #{health2}."
+def time
+  Time.new.strftime("as of %I:%M:%S")
+end
 
-puts title3
-puts title4
+def say_hello(name, health=100)
+  "I'm #{name.capitalize} with a health of #{health} #{time}."
+end
+
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe")
+puts say_hello("shemp", 90)
