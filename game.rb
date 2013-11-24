3bec1c7595a546fe67787deb9651c8b155583d1e
Adam89 <Adamm1@aston.ac.uk>
2013-11-23 19:10:47 -0700

    added basic formatting

--- game.rb
+++ game.rb
@@ -1,9 +1,19 @@
-health = 60
-player_name1 = "larry"
-player_name2 = "curly"
-player_name3 = "moe"
+time = Time.new
+current_time = time.strftime( "The game started on %A %d/%m/%Y %R%p")
+puts current_time
+health1 = 60
+health2 = health1
+health3 = 100
+name1 = "larry".capitalize
+name2 = "curly".upcase
+name3 = "moe".capitalize
+title3 = "#{name3} has a health of #{health3}.".center(50,"*")
+health1 = 30
+health4 = 90
+name4 = "shemp".capitalize
+title4 = "#{name4.ljust(30,".")} #{health4} health"
+puts "#{name1} has a health of #{health1}"
+puts "#{name2} has a health of #{health2}."
 
-puts "#{player_name1}'s health is #{health / 9.0}"
-
-
-puts "Players: \n\t#{player_name1}\n\t#{player_name2}\n\t#{player_name3}"
+puts title3
+puts title4
